bc1d9eb57bc05580580c9bf0f91b96872eaad858
alvarikola <alvaroe484@gmail.com>
2024-11-22 09:54:46 +0000

    funcionalidad de WellnessTaskItem

--- src/main/kotlin/EpisodiosCounter.kt
+++ src/main/kotlin/EpisodiosCounter.kt
@@ -7,8 +7,12 @@ import androidx.compose.runtime.*
 @Composable
 fun EpisodiosCounter() {
     var episodiosContador by remember { mutableStateOf(0) }
+    var mostrarTarea by remember { mutableStateOf(false) }
     Column {
         if (episodiosContador > 0){
+            if (mostrarTarea){
+                WellnessTaskItem("Tienes que verte los episopdios de DanDaDan profe", onClose = { mostrarTarea = false })
+            }
             val episodiosTexto = if (episodiosContador == 1 ) "episodio" else "episodios"
             Text("Te has visto ${episodiosContador} ${episodiosTexto} de DanDaDan")
         }
