53a0e977dd9417f3c2adc0b58dc625f910bf8039
alvarikola <alvaroe484@gmail.com>
2024-11-22 13:46:51 +0000

    añadido checkbox

--- src/main/kotlin/WellnessTaskItem.kt
+++ src/main/kotlin/WellnessTaskItem.kt
@@ -1,14 +1,18 @@
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.material.Checkbox
 
 @Composable
 fun WellnessTaskItem(
@@ -16,12 +20,17 @@ fun WellnessTaskItem(
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val checkedState = remember{ mutableStateOf(true) }
     Row(
         modifier = modifier, verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
             modifier = Modifier.weight(1f).padding(start = 16.dp),
             text = taskName
+        )
+        Checkbox(
+            checked = checkedState.value,
+            onCheckedChange = {checkedState.value = it}
         )
         IconButton(onClick = onClose) {
             Icon(Icons.Filled.Close, contentDescription = "Close")
