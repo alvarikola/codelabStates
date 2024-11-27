49837e6ee9acf77147c6a44f4856a4a606873008
alvarikola <alvaroe484@gmail.com>
2024-11-27 12:53:44 +0000

    quitado mensaje

--- src/main/kotlin/EpisodiosCounter.kt
+++ src/main/kotlin/EpisodiosCounter.kt
@@ -8,12 +8,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 @Composable
 fun EpisodiosCounter() {
     var episodiosContador by rememberSaveable() { mutableStateOf(0) }
-    var mostrarTarea by remember { mutableStateOf(false) }
     Column {
         if (episodiosContador > 0){
-            if (mostrarTarea){
-                WellnessTaskItem("Tienes que verte los episopdios de DanDaDan profe", onClose = { mostrarTarea = false })
-            }
             val episodiosTexto = if (episodiosContador == 1 ) "episodio" else "episodios"
             Text("Te has visto ${episodiosContador} ${episodiosTexto} de DanDaDan")
         }
@@ -21,7 +17,6 @@ fun EpisodiosCounter() {
             Button(
                 onClick = {
                     episodiosContador++
-                    mostrarTarea = true
                 }
             ) {
                 Text("Ver episodio")
