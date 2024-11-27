bce61fcf70065fb10df9ac7e7e5cf6586a58c162
alvarikola <alvaroe484@gmail.com>
2024-11-27 12:43:46 +0000

    actualizado WellnessTaskList.kt

--- src/main/kotlin/WellnessTaskList.kt
+++ src/main/kotlin/WellnessTaskList.kt
@@ -1,4 +1,9 @@
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.modifier.modifierLocalMapOf
 
 
 data class WellnessTask(val id: Int, val label: String)
@@ -7,6 +12,14 @@ fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $1")}
 
 
 @Composable
-fun WellnessTaskList() {
-
+fun WellnessTaskList(
+    listaItems: List<WellnessTask> = remember { getWellnessTasks() }
+) {
+    LazyColumn (
+        state = rememberLazyListState()
+    ){
+        items(listaItems) {
+            WellnessTaskItem(it.label, {})
+        }
+    }
 }
