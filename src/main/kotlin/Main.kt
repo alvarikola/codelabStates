bb95df8a65420b5c92da661107cdb1a6da271b85
alvarikola <alvaroe484@gmail.com>
2024-11-27 12:49:42 +0000

    añadido columna

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -16,8 +17,11 @@ import androidx.compose.ui.window.application
 fun App() {
     MaterialTheme {
         Surface {
-            EpisodiosCounter()
-            WellnessTaskList()
+            Column {
+                EpisodiosCounter()
+                WellnessTaskList()
+            }
+
         }
     }
 }
