e29a1e3502dae1f128dc272587f8200fb87193a0
alvarikola <alvaroe484@gmail.com>
2024-11-29 08:43:48 +0000

    creados archivos para el viewModel

---
+++ src/main/kotlin/WellnessScreen.kt
@@ -0,0 +1,19 @@
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: EjemploViewModel = viewModel()
+) {
+    WellnessTaskList(
+        listaItems = wellnessViewModel.tasks,
+        onCheckedTask = { task, checked ->
+            wellnessViewModel.changeTaskCheked(task, checked)
+        },
+        onCloseTask = { task ->
+            wellnessViewModel.remove(task)
+        }
+    )
+}
